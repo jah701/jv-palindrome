55a096c975989b020be8f227cd17b9977e2b61b3
Jah <moskovienkor@gmail.com>
2020-07-30 14:19:09 +0300

    HOMEWORK-3-3: - done;               - Fixed for checklist test;               - (re) Fixed;

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) {
-        Palindrome pol = new Palindrome();
-        pol.isPalindrome("Madam, I'm Adam!");
-    }
-}
